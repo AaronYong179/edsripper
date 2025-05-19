94d7751c27e4b906ff6bbdc2e46a53ba1844f172
AaronYong179 <aaronyong7922@gmail.com>
2025-05-20 06:08:25 +0800

    further comments

--- extract_eds.py
+++ extract_eds.py
@@ -14,8 +14,12 @@ logging.basicConfig(
 )
 
 class Util:
-    def _convert_index_to_alphanum(well_index):
-        """ This conversion method assumes a 96 well format """
+    def _convert_index_to_alphanum(well_index:str) -> str:
+        """ Converts a well index to its corresponding alphanumeric representation.
+
+        For example, well index 8 = A9 and well index 20 = B9. This conversion method 
+        assumes a 96 well format.
+        """
         column_number = well_index % 12 + 1
         row_alpha = chr(well_index // 12 + 65)
         return f"{row_alpha}{column_number}"
@@ -41,17 +45,16 @@ class Util:
         return L / (1 + np.exp(-k*(x-x0))) + b
 
 class Ripper:
-    def extract_eds_data(eds_path):
+    def extract_eds_data(eds_path:str) -> str:
+        """ Extracts only the raw analysis results from an .eds file. """
 
         logging.info("Preparing to extract analysis_result.txt file from .eds archive.")
 
-        if not os.path.isdir("_temp"):
-            os.mkdir("_temp")
-        shutil.rmtree("_temp")
-
+        # check if master cached_results folder is present for saving the extracted results file
         if not os.path.isdir("cached_results"):
             os.mkdir("cached_results")
 
+        # a quick check to ensure that the input path is actually an .eds file
         basename = os.path.basename(eds_path)
         filename, extension = os.path.splitext(basename)
         try:
@@ -60,14 +63,16 @@ class Ripper:
             logging.error("Input file not of the expected .eds format.")
             raise Exception
         
+        # if the file is already present (by name matching), then skip extraction
         OUTPUT_PATH = os.path.join("cached_results", f"{filename}_analysis_results.txt")
         if os.path.exists(OUTPUT_PATH):
             logging.info("Cached results already exists. Skipping extraction.")
             logging.warning("Make sure that all .eds file names are uniquely identifiable.")
             return OUTPUT_PATH
 
+        # otherwise, extract only the analysis result file.
         with zipfile.ZipFile(eds_path, 'r') as ziphandle:
-            RESULTS_PATH = "apldbio/sds/analysis_result.txt"
+            RESULTS_PATH = "apldbio/sds/analysis_result.txt" # ensure that this matches the expected structure
             with ziphandle.open(RESULTS_PATH) as source, open(OUTPUT_PATH, "wb") as target:
                 target.write(source.read())
 
@@ -121,8 +126,21 @@ class Ripper:
         y_pred = Util._sigmoid(X, *popt) 
         return popt, y_pred
     
-    def get_ct_values(data):
+    def get_ct_values(data:dict) -> list:
+        """ Obtain Ct values (both machine-returned and logistic fitted) and format as a table. 
+        
+        The output is a list of lists, where the first row is a header row. This makes it easy to write to a csv
+        file.
+
+        Args:
+            data (dict) : parsed data from the input .eds file. 
+        Returns:
+            ct_values (list) : table formatted output     
+        """
+
+        # prepare header row 
         ct_values = [["Well", "Sample Name", "Detector", "Avg Ct", "Mid Ct"]]
+        
         for key in data:
             popt, _ = Ripper._fit_logito(data[key]["Delta Rn values"])
             L, x0, k, b = popt # explicit unpacking for clarity
@@ -131,7 +149,8 @@ class Ripper:
             )  
         return ct_values
     
-    def write_rows(output_path, rows):
+    def write_rows(output_path:str, rows:list) -> None:
+        """ Helper method that simply writes a list of lists to the specified output path"""
         with open(output_path, 'w', newline='') as wfile:
             csv.writer(wfile).writerows(rows)
         logging.info(f"Written to output file {output_path}")
