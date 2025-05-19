229814bd70008ed56db21e620c7641e70db3d6ce
AaronYong179 <aaronyong7922@gmail.com>
2025-05-19 16:31:03 +0800

    extract and fix bug with logito

--- Ripper.py
+++ Ripper.py
@@ -1,13 +1,57 @@
-import numpy as np
-from scipy.optimize import curve_fit
-import matplotlib.pyplot as plt
+## Original author: Ian Cheong
+## Modified by: Aaron Yong
 
-def sigmoid(x, L, x0, k, b): 
+import numpy as np
+import os, shutil, zipfile
+from scipy.optimize import curve_fit
+
+def sigmoid(x, L, x0, k, b):
+    """
+    The logistic function is defined as follows:
+    f(x) = [ L / 1 + e^{-k(x - x_0)} ] + b
+
+    where
+        x is the cycle number (i.e, for a standard run, 1 <= x <= 40)
+        L is the maximum signal at plateau
+        x_0 is the cycle number at the midpoint of the curve
+        k is the growth rate
+        b is the minimum signal (baseline)
+
+    Generally, the key parameters of interest should be the following:
+        b, the minimum signal
+        x_0, the cycle number at the midpoint of the curve, 
+        y-gain, defined as L - b (i.e., max - min)
+        PCR efficiency, defined as e^k
+    """
     return L / (1 + np.exp(-k*(x-x0))) + b
 
 class Ripper:
-    def __init__(self, fpath):
-        self.fpath = fpath
+    def __init__(self, eds_path):
+
+        ## Handle extraction first
+        if not os.path.isdir("_temp"):
+            os.mkdir("_temp")
+        shutil.rmtree("_temp")
+
+        if not os.path.isdir("results"):
+            os.mkdir("results")
+        
+        basename = os.path.basename(eds_path)
+        filename, extension = os.path.splitext(basename)
+        assert extension == ".eds"
+
+        with zipfile.ZipFile(eds_path, 'r') as ziphandle:
+            ziphandle.extractall("_temp")
+        
+        analysis_results_path = os.path.join(
+            ".", "_temp", "apldbio", "sds", "analysis_result.txt"
+        )
+        self.fpath = os.path.join(".", "results", f"{filename}_analysis_result.txt")
+        shutil.copy(analysis_results_path, self.fpath)
+
+        ## cleanup
+        shutil.rmtree("_temp")
+
 
     def _convert_index_to_alphanum(self, well_index):
         """ This conversion method assumes a 96 well format """
@@ -15,17 +59,25 @@ class Ripper:
         row_alpha = chr(well_index // 12 + 65)
         return f"{row_alpha}{column_number}"
     
-
     def fit_logito(self, rn_values):
-        X = np.array(range(1, len(rn_values)+1)) # assumed here that each Rn value corresponds to one cycle
-        p0 = [max(rn_values) - min(rn_values), np.median(X), 1, min(rn_values)]
+        """ Fits a logistic (more specifically, a sigmoidal) curve to the raw qPCR data. 
+        
+        """
+
+        # The assumption here is that each Rn value corresponds to one cycle
+        X = np.array(range(1, len(rn_values)+1))
+
+        # A mandatory initial guess
+        p0 = [
+            max(rn_values) - min(rn_values), # guess L
+            np.median(X), # guess x0
+            1, # guess k
+            min(rn_values) # guess b
+        ]
+
         popt, _ = curve_fit(sigmoid, X, rn_values, p0, method="lm")
-        L, x0, k, b = popt
-        y_pred = sigmoid(X, L, x0, k, b)
-        plt.plot(rn_values)
-        plt.plot(y_pred)
-        plt.show()
-        return x0
+        y_pred = sigmoid(X, *popt) 
+        return popt, y_pred
     
     def _default_parse_result_file(self, fhandle):
 
@@ -56,4 +108,24 @@ class Ripper:
                 data[well_key][row[RAW_VALUE_KEY_IDX]] = list(map(float, row[1:]))
 
         return data
+    
+    def get_ct_values(self):
+        
+        with open(self.fpath) as fhandle:
+            data = self._default_parse_result_file(fhandle)
+        
+        ct_values = {}
+        for key in data:
+            
+            popt, _ = self.fit_logito(data[key]["Delta Rn values"])
+            L, x0, k, b = popt # explicit unpacking for clarity
+            
+            ct_values[key] = { 
+                "Avg Ct" : float(data[key]["Avg Ct"]),
+                "Mid Ct" : float(x0)
+            }
+
+        print(ct_values)
+        
+
     
--- sandbox.py
+++ sandbox.py
@@ -1,14 +1,25 @@
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 from Ripper import Ripper
 
 fpath = os.path.join("apldbio", "sds", "analysis_result.txt")
-ripper = Ripper(fpath)
+ripper = Ripper("2025-05-16_104732.eds")
 
-with open(fpath) as fhandle:
-    data = ripper._default_parse_result_file(fhandle)
-    key = "A9"
-    x0 = ripper.fit_logito(data[key]["Delta Rn values"])
-    # plt.plot(data[key]["Delta Rn values"])
-    # plt.axvline(x0)
-    # plt.show()
+# ripper.get_ct_values()
+
+# with open(fpath) as fhandle:
+#     data = ripper._default_parse_result_file(fhandle)
+#     key = "B9"
+#     x0 = ripper.fit_logito(data[key]["Delta Rn values"])
+#     # plt.plot(data[key]["Delta Rn values"])
+#     # plt.axvline(x0)
+#     # plt.show()
+
+# def sigmoid(x, L, x0, k, b): 
+#     return L / (1 + np.exp(-k*(x-x0))) + b
+
+# x = np.linspace(1, 40, 100)
+# plt.plot(x, sigmoid(x, 2.9485, 27.507, 0.87467, 0.01047))
+# plt.axvline(27.507)
+# plt.show()
